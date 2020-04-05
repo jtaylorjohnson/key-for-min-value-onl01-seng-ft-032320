4daaf17e739d80f32e00a0993f4e47b50ef994c5
James Johnson <jtaylorjohnson@gmail.com>
2020-04-05 23:36:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,24 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  new_array = []
+  name_hash.collect do |key, value| 
+   if value[i] < value[0]
+     value[i] = value[x]
+    
 
+  new_array
+  end
+  
+  
+  name_hash.find {|s|}
+  
+  
+  
+  i=0 
+  while i < name_hash.length 
+   name_hash.each_value |n|
+    if n <= n
+   
+  i =+ 1 
 end
